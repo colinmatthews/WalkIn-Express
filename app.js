a05dd4abcb2d27ea199f2e5c876fb490123544e8
colinmatthews <matth11f@uwindsor.ca>
2017-05-02 20:14:41 -0400

    404 redirects to error

--- app.js
+++ app.js
@@ -29,6 +29,25 @@ var NeverBounce = require('neverbounce')({
     apiSecret: 'Yzu8C125gtbYR4F'
 });
 
+app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('error', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
 // ensure https
 app.use(function (req, res, next) {
     var sslUrl;
